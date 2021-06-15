20a452190b55eef31e86a2414ee4d8fee5314242
goodbyemikew <goodbyemikew@outlook.com>
2021-06-15 16:56:42 +0200

    Added audio and image background

--- index.js
+++ index.js
@@ -3,31 +3,54 @@ function sleep(ms) {
 }
 
 async function block() {
-  var text = document.getElementById('loading-text');
-  var block = document.getElementById('blocker');
-  if(localStorage.getItem('reload')) {
+  let text = document.getElementById('loading-text');
+  let block = document.getElementById('blocker');
+  let btnReload = document.getElementById('btn-reload')
+  let timeout = 100;
+  let isPlaying = false;
+  btnReload.addEventListener('click', playMusic);
+
+  if (localStorage.getItem('reload')) {
   	text.innerHTML = 'Ok, thanks, should be working now';
-  	await sleep(5000);
+  	await sleep(timeout);
   	block.remove();
   	return;
   }
 
-  await sleep(5000);
+  await sleep(timeout);
   text.innerHTML = 'Loading...';
-  await sleep(5000);
+  await sleep(timeout);
   text.innerHTML = "Just one more second and that's it";
-  await sleep(5000);
+  await sleep(timeout);
   text.innerHTML = "Almost done, too many people are visiting this page at the moment";
-  await sleep(5000);
+  await sleep(timeout);
   text.innerHTML = "One little png and that's all, I swear";
-  await sleep(5000);
+  await sleep(timeout);
   text.innerHTML = "Sorry, Mike, it was working yesterday";
-  await sleep(5000);
+  await sleep(timeout);
   text.innerHTML = "Google PageSpeed gave me 99";
-  await sleep(5000);
+  await sleep(timeout);
   text.innerHTML = "Somebody, click Reload!!!";
+  btnReload.style.display = 'block';
+}
 
+async function checkLocalStorage({ text, timeout, block }) {
+  if (localStorage.getItem('reload')) {
+  	text.innerHTML = 'Ok, thanks, should be working now';
+  	await sleep(timeout);
+  	block.remove();
+  	return;
+  }
+}
+
+function playMusic() {
+  console.log('trigger')
   localStorage.setItem('reload', 1);
+  let audio = document.getElementById('audio');
+  if (audio.paused) {
+    audio.currentTime = 120;
+    audio.play();
+  }
 }
 
 block();
